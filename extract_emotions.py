93f8fcd649123f94eb3960139aaba6d3285621f6
GrahamRigby <grahamrigbyL@gmail.com>
2019-10-22 18:39:45 -0400

    refactored classification output

--- extract_emotions.py
+++ extract_emotions.py
@@ -2,7 +2,7 @@ import numpy
 import subprocess
 import os
 
-subprocess.call("python3.7 run_classifier.py --load mlstm_semeval.clf --data newsentences.csv --text-key sentence --preprocess --cpu", shell=True)
+#subprocess.call("python3.7 run_classifier.py --load mlstm_semeval.clf --data newsentences.csv --text-key sentence --preprocess --cpu", shell=True)
 sentiment_prob = numpy.load("clf_results.npy.prob.npy")
 corpus = open("newsentences.csv", 'r').readlines()
 output_emotions = open("concat.txt", "w")
@@ -32,32 +32,12 @@ EMOTIONS = {
 
 emotions_idx = {"Mad":0, "Scared":0, "Joyful":0, "Powerful":0, "Peaceful":0, "Sad":0, "Neutral":0}
 for i in range(len(sentiment_prob)):
-    if (max(sentiment_prob[i])>0.4): 
-        e = numpy.argmax(sentiment_prob[i])
-        if (plutchik[e] == plutchik[0]):
-            emotions_idx["Mad"]+=1
-            output_emotions.write('"'+EMOTIONS['Mad'][emotions_idx["Mad"]%len(EMOTIONS['Mad'])]+'",\n')
-        if (plutchik[e] == plutchik[1]) or (plutchik[e] == plutchik[3]):
-            emotions_idx["Scared"]+=1
-            output_emotions.write('"'+EMOTIONS['Scared'][emotions_idx["Scared"]%len(EMOTIONS['Scared'])]+'",\n')
-        if (plutchik[e] == plutchik[2]) or (plutchik[e] == plutchik[5]):
-            emotions_idx["Sad"]+=1
-            output_emotions.write('"'+EMOTIONS['Sad'][emotions_idx["Sad"]%len(EMOTIONS['Sad'])]+'",\n')
-        if (plutchik[e] == plutchik[4]):
-            emotions_idx["Joyful"]+=1
-            output_emotions.write('"'+EMOTIONS['Joyful'][emotions_idx["Joyful"]%len(EMOTIONS['Joyful'])]+'",\n')
-    else:
-        emotions_idx["Neutral"]+=1
-        output_emotions.write('"'+EMOTIONS['Neutral'][emotions_idx["Neutral"]%len(EMOTIONS['Neutral'])]+'",\n')
-
-print(str(emotions_idx["Mad"]) + " Mad")
-print(str(emotions_idx["Scared"]) + " Scared")
-print(str(emotions_idx["Joyful"]) + " Joyful")
-print(str(emotions_idx["Powerful"]) + " Powerful")
-print(str(emotions_idx["Peaceful"]) + " Peaceful")
-print(str(emotions_idx["Sad"]) + " Sad")
-print(str(emotions_idx["Neutral"]) + " Neutral")
-
-
-
-
+  emos_scores = filter(lambda x: x > 0.5, sentiment_prob[i])
+  if len(emos_scores) > 0:
+    emos_scores.sort(reverse = True)
+    emos_idxs = map(lambda x: numpy.where(sentiment_prob[i] == x)[0][0], emos_scores[:2])
+    emos = map(lambda x: plutchik[x], emos_idxs)
+  else:
+    emos = []
+  print(emos)
+  output_emotions.write('"'+str(corpus[i+1])[:-1]+'"'+': '+str(emos)+',\n')
