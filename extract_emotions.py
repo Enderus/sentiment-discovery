74b8958b5017e890d3f5093b162e94c5bd54dc8f
GrahamRigby <grahamrigbyL@gmail.com>
2019-10-25 14:19:14 -0400

    refactored extract emotions

--- extract_emotions.py
+++ extract_emotions.py
@@ -2,35 +2,13 @@ import numpy
 import subprocess
 import os
 
-#subprocess.call("python3.7 run_classifier.py --load mlstm_semeval.clf --data newsentences.csv --text-key sentence --preprocess --cpu", shell=True)
+#Runs the classifier, see arguments.py for more information. The arguments here load the LSTM classifier (link in README.md), uses the corpus: newsentences.csv, runs preprocessing, and runs on the cpu. If you have a gpu in your machine remove the --cpu flag.
+subprocess.call("python3.7 run_classifier.py --load mlstm_semeval.clf --data newsentences.csv --text-key sentence --preprocess --cpu", shell=True)
 sentiment_prob = numpy.load("clf_results.npy.prob.npy")
 corpus = open("newsentences.csv", 'r').readlines()
 output_emotions = open("concat.txt", "w")
 plutchik = ["anger", "anticipation", "disgust", "fear", "joy", "sadness", "surprise", "trust"]
 
-EMOTIONS = {
-  "Mad": ["Someone cut in front of you in line", 
-        "You just stubbed your toe", 
-        "Your rival got a job you applied for"],
-  "Scared": ["You're alone and you hear a concerning noise", 
-          "You're lost in an unfamiliar area", 
-          "You're speaking to an audience and experieincing stagefright."],
-  "Joyful": ["You're having fun at a comedy show'", 
-          "Your crush smiles at you", 
-          "You just received an offer for your dream job"],
-  "Powerful": ["You just aced an exam", 
-             "You just defeated your rival in a game", 
-             "You're wearing an outfit that makes you feel professional"],
-  "Peaceful": ["You just had a relaxing massage", 
-             "Your friends gave you a warm hug", 
-             "You are enjoying a nice meal with your mother"],
-  "Sad": ["You just failed an exam", 
-        "Your friends are having fun at a concert you weren't invited to", 
-        "You're up late at night doing homework"],
-  "Neutral": ["Use a neutral tone"]
-}
-
-emotions_idx = {"Mad":0, "Scared":0, "Joyful":0, "Powerful":0, "Peaceful":0, "Sad":0, "Neutral":0}
 for i in range(len(sentiment_prob)):
   emos_scores = filter(lambda x: x > 0.5, sentiment_prob[i])
   if len(emos_scores) > 0:
@@ -38,6 +16,5 @@ for i in range(len(sentiment_prob)):
     emos_idxs = map(lambda x: numpy.where(sentiment_prob[i] == x)[0][0], emos_scores[:2])
     emos = map(lambda x: plutchik[x], emos_idxs)
   else:
-    emos = []
-  print(emos)
+    emos = ["neutral"]
   output_emotions.write('"'+str(corpus[i+1])[:-1]+'"'+': '+str(emos)+',\n')
